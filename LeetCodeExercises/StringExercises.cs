f8fa18420fb3ac6c66bdc9cdc4402815c2d0e706
L3xer <sanya2371@gmail.com>
2018-03-12 17:26:18 +0200

    Add Reverse string.

---
+++ LeetCodeExercises/StringExercises.cs
@@ -0,0 +1,13 @@
+﻿using System.Linq;
+
+namespace LeetCodeExercises
+{
+    public static class StringExercises
+    {
+        // Reverse string
+        public static string ReverseString(string s)
+        {
+            return new string(s.ToCharArray().Reverse().ToArray());
+        }
+    }
+}
---
+++ UnitTests/StringTests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using LeetCodeExercises;
+
+
+namespace UnitTests
+{
+    public class StringTests
+    {
+        [Theory]
+        [InlineData("hello", "olleh")]
+        public void ReverseStringTest(string s, string expected)
+        {
+            string actual = StringExercises.ReverseString(s);
+            Assert.Equal(expected, actual);
+        }
+    }
+}
