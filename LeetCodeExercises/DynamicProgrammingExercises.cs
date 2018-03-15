cf11c11a3ab767c04cc00fef5a9db8e43aeffb28
L3xer <sanya2371@gmail.com>
2018-03-15 14:54:48 +0200

    Add Jump Game.

--- LeetCodeExercises/DynamicProgrammingExercises.cs
+++ LeetCodeExercises/DynamicProgrammingExercises.cs
@@ -1,6 +1,21 @@
-﻿namespace LeetCodeExercises
+﻿using System;
+
+namespace LeetCodeExercises
 {
     public static class DynamicProgrammingExercises
     {
+        public static bool CanJump(int[] nums)
+        {
+            int distance = 0;
+            int lastIndex = nums.Length - 1;
+
+            for (int i = 0; i <= distance; i++) {
+                distance = Math.Max(distance, nums[i] + i);
+                if (distance >= lastIndex)
+                    return true;
+            }
+
+            return false;
+        }
     }
 }
--- UnitTests/DynamicProgrammingTests.cs
+++ UnitTests/DynamicProgrammingTests.cs
@@ -1,6 +1,22 @@
-﻿namespace UnitTests
+﻿using LeetCodeExercises;
+using Xunit;
+
+namespace UnitTests
 {
     public class DynamicProgrammingTests
     {
+        [Theory]
+        [InlineData(new int[] { 2, 3, 1, 1, 4 }, true)]
+        [InlineData(new int[] { 3, 2, 1, 0, 4 }, false)]
+        [InlineData(new int[] { 2, 5, 0, 0}, true)]
+        [InlineData(new int[] { 1, 0, 1, 1 }, false)]
+        [InlineData(new int[] { 0 }, true)]
+        [InlineData(new int[] {5, 4, 0, 2, 0, 1, 0, 1, 0 }, false)]
+        public void CabJumpTest(int[] nums, bool expected)
+        {
+            bool actual = DynamicProgrammingExercises.CanJump(nums);
+
+            Assert.Equal(expected, actual);
+        }
     }
 }
